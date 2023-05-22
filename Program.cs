41de07e555d1a7892c208e9406915a9448876058
SargsyanRuben <134294986+SargsyanRuben@users.noreply.github.com>
2023-05-22 17:35:57 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace SHAXMAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Users users = new Users();
+            while (true)
+            {
+                users.User1();
+                users.User2();
+            }
+            
+        }
+    }
+}
